44aaef76528b7bc5180ed2ce1379806a9ed6b45d
Emir Türkmen <s353921@studenti.polito.it>
2026-06-05 11:03:02 +0200

    Added db initialization

--- server/db.js
+++ server/db.js
@@ -1,11 +1,29 @@
 import sqlite from 'sqlite3';
+import fs from 'fs';
 // Open the connection with db
 const db = new sqlite.Database('lastrace.sqlite', (err) => {
     if (err) throw err;
 });
 
-//
+// Enable foreign key constraints
 db.run('PRAGMA foreign_keys = ON', (err) => {
     if (err) throw err;
-})
+});
+
+// Read schema.sql file
+const sql = fs.readFileSync('schema.sql', 'utf8');
+
+// Execute sql commands in schema.sql
+db.exec(sql, (err) => {
+    if (err) throw err;
+    console.log('Schema created');
+
+    const seedSql = fs.readFileSync('seed.sql', 'utf8');
+    db.exec(seedSql, (err) => {
+        if (err) throw err;
+        console.log("Seed created successfully.");
+    })
+});
+
+
 export default db;
