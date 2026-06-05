bfdb2295096fc1121c2db3a3bfddb1ba3c9ec153
Emir Türkmen <s353921@studenti.polito.it>
2026-06-05 11:25:05 +0200

    Created index.js to initialize backend app

--- server/index.js
+++ server/index.js
@@ -1,9 +1,18 @@
 // imports
 import express from "express";
+import cors from "cors";
+import morgan from "morgan";
 
 // init express
-const app = new express();
+const app = express();
 const port = 3001;
+app.use(morgan('dev'));
+app.use(express.json());
+// use cors to prevent browser from blocking requests to backend as there are 2 servers
+app.use(cors({
+  origin: 'http://localhost:5173',  // allow only this origin
+  credentials: true                 // necessary for cookies
+}));
 
 // activate the server
 app.listen(port, () => {
